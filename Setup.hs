7c5fe650aa674f6e62cfdd9b01e5742b43f5ecc2
Jude Taylor <me@jude.xyz>
2018-06-30 16:58:32 -0700

    fix Setup.hs

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,19 @@
-module Main where
+{-# LANGUAGE CPP #-}
 
+#ifndef MIN_VERSION_cabal_doctest
+#define MIN_VERSION_cabal_doctest(x,y,z) 0
+#endif
+
+module Main (main) where
+
+#if MIN_VERSION_cabal_doctest(1,0,0)
 import Distribution.Extra.Doctest (defaultMainWithDoctests)
+#else
+#ifdef MIN_VERSION_Cabal
+import Warning ()
+#endif
+import Distribution.Simple
+defaultMainWithDoctests _ = defaultMain
+#endif
 
-main :: IO ()
 main = defaultMainWithDoctests "doctest"
